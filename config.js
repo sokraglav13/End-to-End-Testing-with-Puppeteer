1ffcfcc21ab993178852349545f33f3da80ff93b
sokgla12 <sokraglav@gmail.com>
2024-10-16 10:24:15 +0300

    10_16_2024 Fix README file

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     /**Sets the base url for testing */
     baseUrl: "https://www.saucedemo.com/",
 
-    /**Sets the text execution timeout (0 = no timeout) */
+    /**Sets the test execution timeout (0 = no timeout) */
     timeoutTest: 0,
 
     /**Sets the commands timeout */
